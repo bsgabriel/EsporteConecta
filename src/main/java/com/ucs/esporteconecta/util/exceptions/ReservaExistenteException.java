f046dc0bba28c822bec516d4845175bcac940fb7
bsgabriel <gabrielbs22@icloud.com>
2023-10-14 15:05:48 -0300

    Criado exception para quando houver uma Reserva já cadastrada

---
+++ src/main/java/com/ucs/esporteconecta/util/exceptions/ReservaExistenteException.java
@@ -0,0 +1,7 @@
+package com.ucs.esporteconecta.util.exceptions;
+
+public class ReservaExistenteException extends Exception {
+    public ReservaExistenteException(String message) {
+        super(message);
+    }
+}
